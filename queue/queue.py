a4ebf2aff792eb6a25c37952331ed0f81ea46226
adamhinckley <adamhinckley@mac.com>
2019-02-25 13:00:49 -0700

    init

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,17 @@
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements?
+        self.storage =
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        # add item to que
 
-  def len(self):
-    pass
+        pass
+
+    def dequeue(self):
+        pass
+
+    def len(self):
+        pass
