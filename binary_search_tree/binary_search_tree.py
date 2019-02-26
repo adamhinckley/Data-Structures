8d786f088edddcfe1c5cdbfd5f8b1341e0f5ac52
adamhinckley <adamhinckley@mac.com>
2019-02-26 14:02:04 -0700

    scraps original pseudo code for insert

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,13 +5,6 @@ class BinarySearchTree:
         self.right = None
 
     def insert(self, value):
-        # if there is no value; self.value is None
-        # use while loop
-        # if self.value == value; finish the loop
-        # if self.value > value and self.left is not None; self.value = self.left
-        # else self.left = self.value; finish the loop
-        # else if self.right is not None; self.value = self.left
-        # else self.right = self.value; finish the loop
 
     def contains(self, target):
         pass
